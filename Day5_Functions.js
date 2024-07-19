79231adc7161823a533d005a4d2d1de60440cdb0
MitaliM13 <mitalimaurya@kccemsr.edu.in>
2024-07-19 15:35:27 +0530

    Start Day 5 Task

---
+++ Day5_Functions.js
@@ -0,0 +1,26 @@
+//Activity 1: Function Declaration
+
+//Task 1: Check Whether Even or Odd
+function EvenOrOdd(num) {
+    if (num % 2 == 0) {
+        console.log("Number is Even",num);
+    } else {
+        console.log("Number is Odd", num);
+    }
+}
+
+// console.log(EvenOrOdd(32));
+
+//Task 2: Calculate Square of a number
+
+function SquareNum(Num) {
+    let sqr = Num*Num
+    console.log(`Square of Number ${Num} is:`, sqr);
+}
+
+console.log(SquareNum(5));
+
+//Activity 2: Function Expression
+
+//Task 3: Max of 2 Numbers
+

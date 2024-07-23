2e11a870b78192fce9eca0aa8cb38b1e1c15cddc
MitaliM13 <mitalimaurya@kccemsr.edu.in>
2024-07-23 16:51:52 +0530

    Day 11 task done

--- Day11_Promise.js
+++ Day11_Promise.js
@@ -1,6 +1,8 @@
 //Activity 1: Understanding Promises
 //Task 1: Create a promise that resolves with the message after a 2 second timeout and log the message to the console
 
+const { data } = require("autoprefixer")
+
 // const promise = new Promise((resolve,reject) => {
 //     setTimeout(() => {
 //         const message = "Promise Resolved"
@@ -117,24 +119,97 @@ Chain the promises to the log message in a specific order*/
 
 //Task 5: Write an async function that handles a rejected promise using try-catch, log the error message
 
-const checkAge = (age) => {
-    return new Promise((res, reject) => {
-        if(age>18){
-            res(`Eligible to vote ${age}`)
-        } else {
-            reject(`Not eligible`)
-        }
-    })
-}
+// const checkAge = (age) => {
+//     return new Promise((res, reject) => {
+//         if(age>18){
+//             res(`Eligible to vote ${age}`)
+//         } else {
+//             reject(`Not eligible`)
+//         }
+//     })
+// }
 
-const handleAgeCheck = async(age) => {
-    try{
-        const message = await checkAge(age)
-        console.log(`Promise resolved`);
-    } catch (error) {
-        console.log(`Promise Rejected ${error}`);
-    }
-}
+// const handleAgeCheck = async(age) => {
+//     try{
+//         const message = await checkAge(age)
+//         console.log(`Promise resolved`);
+//     } catch (error) {
+//         console.log(`Promise Rejected ${error}`);
+//     }
+// }
 
-handleAgeCheck(20)
-handleAgeCheck(7)
+// handleAgeCheck(20)
+// handleAgeCheck(7)
+
+//Activity 4: Fetching data from API
+//Task 6: Use fetch API to get data from a public API and log the response data to the console using promises
+
+// const apiUrl = 'https://api.github.com/users/hiteshchoudhary'
+
+// function fetchData() {
+//     fetch(apiUrl)
+//     .then(response => {
+//         if(!response.ok){
+//             throw new Error(`Network response was NOT OK`)
+//         }
+//         return response.json()
+//     })
+//     .then(data => {
+//        console.log(`Response Data:`,data); 
+//     })
+//     .catch(error => {
+//         console.error(`There was a problem with the fetch operation: ${error}`);
+//     })
+// }
+
+// fetchData()
+
+//Task 7: Use fetch API to get data from a public API and log the response data to the console using async await
+
+// const apiUrl = 'https://api.github.com/users/hiteshchoudhary'
+
+
+// async function fetchData(){
+//     try{
+//         const data = await fetch(apiUrl)
+//         if(!data.ok){
+//             throw new Error(`Response not ok`)
+//         }
+//         const response = await data.json()
+
+//         console.log(`Response Data`, response);
+//     } catch(error){
+//         console.error(`There was a problem with fetch operation`);
+//     }
+// }
+
+// fetchData()
+
+//Activity 5: Concurrent Promises
+//Task 8: Use Promise.All to wait for multiple promises to resolve and then log all thier values
+
+// const promise1 = Promise.resolve(7)
+// const promise2 = 2013
+// const promise3 = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve(`Bangtan`)
+//     },2000)
+// })
+
+// Promise.all([promise1,promise2,promise3]).then((value) => {
+//     console.log(value);
+// })
+
+//Task 9: Use Promise.race to log the value of the first promise that resolves among multiple promises
+
+const promise1 = new Promise((res,rej)=>{
+    setTimeout(res, 50 , 'Hello')
+})
+const promise2 = new Promise((res,rej)=>{
+    setTimeout(res, 100 , 'Bye')
+})
+
+Promise.race([promise1,promise2]).then((value)=>{
+    console.log(value);
+})
+
